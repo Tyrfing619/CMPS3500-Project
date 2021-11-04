15670da62fdcd5b00799c129b2630154cdf68271
Stephen Christian Carlos <scarlos@odin.cs.csubak.edu>
2021-11-03 23:23:34 -0700

    Updated version, should be a whole lot cleaner

--- CMPS3500_Project_Part_1.py
+++ CMPS3500_Project_Part_1.py
@@ -8,11 +8,15 @@ data = []
 with open(FILENAME, newline='') as csvfile:
     csvreader = csv.reader(csvfile)
     header = next(csvreader)
-    print(header)
+    for cat in header:
+        print(cat, end="\t\t\t")
+    print("")
     for row in csvreader:
         data.append(row)
 
 csvfile.close()
 for i in range(100):        #can be written as for i in range data:   print(i), but too many results so far
-	print(data[i])
-	
+    for val in data[i]:
+        print(val, end="\t\t\t\t")
+    print("")                                         
+
